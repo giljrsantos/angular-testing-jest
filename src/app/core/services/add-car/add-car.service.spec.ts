67a2688daac8633c5e237842c7c27143cca02cfb
Gilberto Junior <giljrsantos@gmail.com>
2025-02-09 19:49:56 -0300

    feat: implementation functions service add car

--- src/app/core/services/add-car/add-car.service.spec.ts
+++ src/app/core/services/add-car/add-car.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { AddCarService } from './add-car.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { mockCar } from '../../../../assets/mock/m-add-car.mock';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 
 describe('AddCarService', () => {
   let service: AddCarService;
@@ -16,22 +16,22 @@ describe('AddCarService', () => {
     httpClient = TestBed.inject(HttpClient);
   });
 
-  it('should be created', () => {
+  it('deve criar', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should have addCar function', () => {
+  it('deve chamar a função addCar()', () => {
     expect(service.addCar).toBeTruthy();
   });
 
-  it('should add car', () => {
+  it('deve adicionar um carro', () => {
     service.addCar(mockCar).subscribe((res) => {
       expect(res).toEqual(mockCar);
     });
   });
 
-  it('should  have addCar function method POST', () => {
-    const httpSpy = jest.spyOn(httpClient, 'post').mockReturnValue(of(mockCar));
+  it('deve chamar o método de função addCar() POST', () => {
+    const httpSpy = jest.spyOn(httpClient, 'post').mockReturnValue(of({}));
     service.addCar(mockCar).subscribe(() => {
       expect(httpSpy).toHaveBeenCalledWith('http://localhost:3000/add-car', mockCar);
       expect(httpSpy).toHaveBeenCalledTimes(1);
@@ -39,9 +39,19 @@ describe('AddCarService', () => {
     });
   });
 
-  it('should call http post with body car', () => {
+  it('deve chamar http post para adicionar os dados do carro', () => {
+    jest.spyOn(httpClient, 'post');
     service.addCar(mockCar).subscribe((res) => {
       expect(res).toEqual(mockCar);
+    });
+  });
+
+  it('deve lidar com erro quando adicionar carro falhar', () => {
+    const error = new Error('error');
+    const spy = jest.spyOn(httpClient, 'post');
+    spy.mockReturnValueOnce(throwError(() => error));
+    service.addCar(mockCar).subscribe((res) => {
+      expect(res).toBeUndefined();
     });
   });
 
