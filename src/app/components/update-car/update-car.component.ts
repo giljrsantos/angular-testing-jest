eaee39b3526e185e6329f60f55888fd05a58437a
Gilberto Junior <giljrsantos@gmail.com>
2025-02-15 20:24:26 -0300

    feat: adicionar componente UpdateCar com template, estilos e testes

---
+++ src/app/components/update-car/update-car.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-car',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-car.component.html',
+  styleUrl: './update-car.component.scss'
+})
+export class UpdateCarComponent {
+
+}
